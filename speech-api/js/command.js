7e54a7a293365224db9cee8a62a1795e6d79f7d0
Florian Csizmazia <florian.csizmazia@gmail.com>
2014-11-17 16:32:02 +0100

    added possibility to save commands in backbone

---
+++ speech-api/js/command.js
@@ -0,0 +1,20 @@
+/**
+ * Created by floe on 15.10.14.
+ */
+
+var projectham = projectham || {};
+
+projectham.Command = Backbone.Model.extend({
+    defaults: function() {                             // every object that is created has these two attributes
+        return {
+            command: '',
+            time: new Date().toLocaleTimeString()
+        }
+    },
+
+    initialize: function() {
+        this.on('change', function() {
+            console.log('Model has changed');
+        })
+    }
+});
